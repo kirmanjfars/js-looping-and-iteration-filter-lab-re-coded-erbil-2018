76fb260b7ff8583c5b9c113a51118a1e2259ab10
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-21 17:40:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file
+
+const drivers=[];
+
+
+function findMatching(list , name){
+  
+  return list.filter( function(s){
+      if(s.toLowerCase() == name.toLowerCase()){
+        return s;
+      }else{
+        return undefined;
+      }
+    } );
+}
+
+function fuzzyMatch (list, partialName) {
+  
+  return list.filter(
+    function(s){
+      for(let i =0; i<(s.length - 2); i++){
+         if(partialName == s.slice(i, i+2)){
+           return s;
+         }else{
+ 
+           return null;
+         }
+      }
+    }
+    );
+}
+
+
+
+function matchName(list , name){
+  
+  return list.filter( function(s){
+      if(s.name == name){
+        return s;
+      }else{
+        return undefined;
+      }
+    } );
+}
+
+
+
